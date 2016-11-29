bd162598d0ec2968eb65d5220522f12ed9e79de1
Krzysztof Kirsz <k.kirsz@gmail.com>
2016-11-29 23:07:49 +0100

    initial commit - impress + jquerry included

---
+++ js/first_steps.js
@@ -0,0 +1,19 @@
+impress().init();
+/*
+var imp=impress();
+var timeout_val = 5000;
+var advance_timeout = setTimeout(advance, timeout_val);
+
+function run_timeout()
+{
+  clearTimeout(advance_timeout);
+  advance_timeout = setTimeout(advance, timeout_val);
+}
+
+function advance()
+{
+  imp.next();
+  run_timeout();
+}
+
+$("#impress").on("impress:stepenter", run_timeout);*/
